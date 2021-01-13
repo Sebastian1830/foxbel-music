69961e49d09abf401900b6e1119999af949b117a
sebastian1830 <rsotov1@upao.edu.pe>
2021-01-13 01:08:06 -0500

    auth deezer

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,16 @@
 import Vue from 'vue';
-import VueRouter, { RouteConfig } from 'vue-router';
+import Router, { RouteConfig } from 'vue-router';
 import Login from '../views/Login.vue';
 import RecentPage from '../views/RecentPage.vue';
 
-Vue.use(VueRouter);
+Vue.use(Router);
 
 const routes: Array<RouteConfig> = [
   { path: '/', name: 'Login', component: Login },
   { path: '/recent-page/', name: 'recent-page', component: RecentPage, props: true },
 ];
 
-const router = new VueRouter({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
